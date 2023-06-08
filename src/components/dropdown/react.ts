516ad1ddf1ad938590f7dc4091983f11fbfe5023
Aymen <labidi@aymen.co>
2023-06-08 17:03:31 +0100

    fix: type in react wrapper dropdown

--- src/components/dropdown/react.ts
+++ src/components/dropdown/react.ts
@@ -2,7 +2,7 @@ import {createComponent} from '@lit-labs/react';
 import * as React from 'react';
 import {HyDropdownComponent} from './hy-dropdown.component';
 export const HyDropdown = createComponent({
-  tagName: 'hy-dropdwon',
+  tagName: 'hy-dropdown',
   elementClass: HyDropdownComponent,
   react: React,
   events: {},
