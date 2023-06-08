e1afc8bb5569bdd17ef3d9da4ab57601641c5a9f
Aymen <labidi@aymen.co>
2023-06-08 15:08:16 +0100

    fix: react wrapper

--- src/components/dropdown/react.ts
+++ src/components/dropdown/react.ts
@@ -1,7 +1,7 @@
 import {createComponent} from '@lit-labs/react';
 import * as React from 'react';
 import {HyDropdownComponent} from './hy-dropdown.component';
-export const HyDatepicker = createComponent({
+export const HyDropdown = createComponent({
   tagName: 'hy-dropdwon',
   elementClass: HyDropdownComponent,
   react: React,
